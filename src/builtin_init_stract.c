d19beea15b025b0e80f0399e5667d5598249b407
admin <admin@MacBook-Pro-admin.local>
2020-07-23 00:04:23 +0300

    whith /. && ////dev

--- src/builtin_init_stract.c
+++ src/builtin_init_stract.c
@@ -2,8 +2,9 @@
 
 void mx_init (t_app *pwd) {
     pwd->home = getenv("HOME");
-    pwd->pwd_l = getenv("PWD");
+    pwd->pwd_l = mx_strdup(getenv("PWD"));
+    pwd->old_pwd_l = mx_strdup(pwd->pwd_l);
     pwd->pwd_p = getwd(NULL);
-    pwd->old_pwd_p = pwd->pwd_p;
+    pwd->old_pwd_p = mx_strdup(pwd->pwd_p);
 }
 
